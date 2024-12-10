83979e020281696ad855833d8b5fc8ac61d4578a
Atahan <72799577+ataozturk@users.noreply.github.com>
2024-12-10 15:35:02 +0300

    Add files via upload
    Lab ödev

---
+++ student-management/src/main/java/com/example/student_management/StudentService.java
@@ -0,0 +1,28 @@
+package com.example.student_management;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void saveStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
